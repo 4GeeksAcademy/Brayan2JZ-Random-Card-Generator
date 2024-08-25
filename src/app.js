e9384a93f7884b059d741d2353e0fffc9680bea3
Brayan2JZ <b.sredzinski92@gmail.com>
2024-08-25 18:20:21 +0000

    Commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,26 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let numbers = [2, 3, 4, 5, 6, 7, 8, 9, "A", "J", "Q", "K"];
+let symbols = ["♣", "♠", "♥", "♦"];
+
+function genCard() {
+  let number = numbers[Math.floor(Math.random() * numbers.length)];
+  let symbol = symbols[Math.floor(Math.random() * symbols.length)];
+
+  let symbolColor;
+  if (symbol === "♥" || symbol === "♦") {
+    symbolColor = "red";
+  } else {
+    symbolColor = "black";
+  }
+
+  document.getElementById(
+    "top"
+  ).innerHTML = `<span style="color: ${symbolColor};">${symbol}</span>`;
+  document.getElementById("mid").innerHTML = number;
+  document.getElementById(
+    "bot"
+  ).innerHTML = `<span style="color: ${symbolColor};">${symbol}</span>`;
+}
+window.onload = genCard;
